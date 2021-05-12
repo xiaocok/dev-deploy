92954e56867e3a40efab5df81c56b83cbb354ca1
gitteamer <shengbo321@163.com>
2021-05-13 00:09:31 +0800

    consul

---
+++ docker/consul/run-progrium_consul.sh
@@ -0,0 +1,10 @@
+
+docker run -d \
+	-p 53:53 \
+	-p 8300:8300 \
+	-p 8400:8400 \
+	-p 8500:8500 \
+	-e SERVICE_TAGS="consul servers" \
+	progrium/consul:latest -server -ui-dir /ui -data-dir /tmp/consul -bootstrap-expect 1
+
+# https://hub.docker.com/r/progrium/consul
--- docker/consul/run.sh
+++
@@ -1,2 +0,0 @@
-
-docker run --name consul -d -p 8500:8500 consul
